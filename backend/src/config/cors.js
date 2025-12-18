b663ed52f817520f838c0d40049b83d5055b7f96
Inaros Lemuel <infromke@gmail.com>
2025-12-18 16:30:53 -0300

    add cors options

---
+++ backend/src/config/cors.js
@@ -0,0 +1,17 @@
+import cors from 'cors'
+
+const allowedOrigins = [`http://localhost:${process.env.CLIENT_PORT}`]
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS!'))
+    }
+  },
+  methods: 'GET,POST',
+  credentials: true,
+}
+
+export default cors(corsOptions)
