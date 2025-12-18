c53b03225876be268fc2fd5f1c5f404d2adab2d3
Inaros Lemuel <infromke@gmail.com>
2025-12-18 16:32:18 -0300

    update general setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,31 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+
 import connectToDb from './config/database.js'
+import cors from './config/cors.js'
+import { verifyConnection } from './config/nodemailer.js'
+
+import errorHandler from './middlewares/errorHandler.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 connectToDb()
+verifyConnection() // verifica a conexão do nodemailer
+
+app.use(express.json())
+app.use(cors)
+app.use(cookieParser())
+
+//  middleware para rotas não encontradas (404)
+app.use((req, res, next) => {
+  const error = new Error('Route not found')
+  error.status = 404
+  next(error)
+})
+
+//  middleware de erro global
+app.use(errorHandler)
 
 app.listen(PORT, () =>
   console.log(`[SERVER] up and running at http://localhost:${PORT}`)
