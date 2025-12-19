cbcbbe43213f9887c9d3321b5554c5853f9447f3
Inaros Lemuel <infromke@gmail.com>
2025-12-19 16:54:55 -0300

    update: signs whole object instead of just id

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from 'jsonwebtoken'
 
 /**
  * Gera um token JWT.
- * @param {string} _id - ObjectId do usuário.
+ * @param {object} user - Objeto contendo dados do usuário.
  * @param {string} secret - Chave secreta utilizada para assinar o token. Deve sem uma string longa e aleatória.
  * @param {number} expirationTime - Tempo de expiração do token expresso em segundos ou uma string descrevendo um intervalo de tempo. Ex.: '120ms', '10h', '2d' ou apenas um número.
  * @returns {string} token para o usuário especificado.
  */
-const generateToken = ({ _id }, secret, expirationTime) => {
-  return jwt.sign({ id: _id }, secret, {
+const generateToken = (user, secret, expirationTime) => {
+  return jwt.sign(user, secret, {
     expiresIn: expirationTime,
   })
 }
