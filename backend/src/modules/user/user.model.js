72431e4424f1e5aacae222f5805123765b467ba0
Inaros Lemuel <infromke@gmail.com>
2025-12-20 22:50:45 -0300

    add pre middleware for findOneAndUpdate

--- backend/src/modules/user/user.model.js
+++ backend/src/modules/user/user.model.js
@@ -33,6 +33,23 @@ userSchema.pre('save', async function (next) {
   }
 })
 
+//  faz a senha ser hasheada nas operações User.findByIdAndUpdate() e User.findOneAndUpdate()
+userSchema.pre('findOneAndUpdate', async function (next) {
+  // pega o objeto do "update" ex.:{ password: 'nova_senha' })
+  const update = this.getUpdate()
+
+  // verifica se a senha está sendo atualizada no objeto
+  if (update.password) {
+    try {
+      const hash = await generatePassword(update.password)
+      update.password = hash
+    } catch (error) {
+      next(error)
+    }
+  }
+  next()
+})
+
 const User = mongoose.model('User', userSchema)
 
 export default User
