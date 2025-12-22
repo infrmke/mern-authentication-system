b6e66b07c7ff0a70b3d59b2de68f3a049d2e9a16
Inaros Lemuel <infromke@gmail.com>
2025-12-22 16:56:47 -0300

    add navbar component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar'
+
+import './styles/base.css'
+import './styles/typography.css'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
