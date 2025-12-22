bed4d7c88561580af9096f706f69c4a94bfb67a4
Inaros Lemuel <infromke@gmail.com>
2025-12-22 18:50:57 -0300

    create browser router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
 
 import App from './App.jsx'
+import LogIn from './routes/LogIn.jsx'
+import SignUp from './routes/SignUp.jsx'
+import Home from './routes/Home.jsx'
+import ForgotPassword from './routes/ForgotPassword.jsx'
+
 import './styles/index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LogIn />,
+  },
+  {
+    path: '/register',
+    element: <SignUp />,
+  },
+  {
+    path: '/home',
+    element: <App />,
+    children: [{ path: '/home', element: <Home /> }],
+  },
+  { path: '/password', element: <ForgotPassword /> },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
