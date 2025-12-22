e702879ca77db552b2db61eedd3abf9f9445ef07
Inaros Lemuel <infromke@gmail.com>
2025-12-22 16:56:27 -0300

    create navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import '../styles/navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__logo">
+        <p>auth</p>
+      </div>
+
+      <div className="nav__user">
+        <p>M</p>
+        <p>Mock</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
