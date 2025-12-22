248cc71ae5c5b89784dfaa915735b57c25350cf1
Inaros Lemuel <infromke@gmail.com>
2025-12-21 23:46:28 -0300

    integrate validator middleware

--- backend/src/modules/otp/otp.route.js
+++ backend/src/modules/otp/otp.route.js
@@ -1,6 +1,11 @@
 import { Router } from 'express'
 
 import OtpController from './otp.controller.js'
+import {
+  otpValidator,
+  emailValidator,
+  passwordResetValidator,
+} from './otp.validator.js'
 
 import validateIdFormat from '../../middlewares/validateIdFormat.js'
 import validateIdExists from '../../middlewares/validateIdExists.js'
@@ -8,11 +13,20 @@ import validateIdExists from '../../middlewares/validateIdExists.js'
 const router = Router()
 
 const validateId = [validateIdFormat, validateIdExists]
+const validateIdAndOtp = [...validateId, otpValidator]
 
 router.post('/email/:id', validateId, OtpController.sendEmailVerification)
-router.post('/email/verify/:id', validateId, OtpController.verifyUserEmail)
+router.post(
+  '/email/verify/:id',
+  validateIdAndOtp,
+  OtpController.verifyUserEmail
+)
 
-router.post('/reset', OtpController.sendPasswordReset)
-router.post('/reset/verify/', OtpController.resetUserPassword)
+router.post('/reset', emailValidator, OtpController.sendPasswordReset)
+router.post(
+  '/reset/verify/',
+  passwordResetValidator,
+  OtpController.resetUserPassword
+)
 
 export default router
