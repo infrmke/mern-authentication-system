719184fadcb06b3edc169bcf0f07ad85c4ba005b
Inaros Lemuel <infromke@gmail.com>
2025-12-22 18:30:28 -0300

    add home page

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome, [user]!</h1>
+      <p>Could you verify your e-mail?</p>
+      <button className="btn">Verify e-mail</button>
+    </div>
+  )
+}
+
+export default Home
