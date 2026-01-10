1aabecf7d0c3abd85cb41c5961e1225194dcf6d5
Inaros Lemuel <infromke@gmail.com>
2026-01-10 05:31:11 -0300

    fix: ensure user id comparison is done as a string

--- backend/src/middlewares/verifyOwnership.js
+++ backend/src/middlewares/verifyOwnership.js
@@ -2,7 +2,7 @@
  * Verifica se o usuário autenticado é o proprietário da conta passada pelo ID na URL.
  */
 const verifyOwnership = (req, res, next) => {
-  if (req.params.id !== req.user.id) {
+  if (req.params.id !== req.user.id.toString()) {
     return res
       .status(403)
       .json({ message: 'You can only modify your own account.' })
