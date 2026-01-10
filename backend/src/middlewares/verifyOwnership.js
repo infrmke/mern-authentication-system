edc89e7b84d080ca48e2853e79fe1d00070b8e58
Inaros Lemuel <infromke@gmail.com>
2026-01-10 03:07:34 -0300

    create verifyOwnership middleware

---
+++ backend/src/middlewares/verifyOwnership.js
@@ -0,0 +1,13 @@
+/**
+ * Verifica se o usuário autenticado é o proprietário da conta passada pelo ID na URL.
+ */
+const verifyOwnership = (req, res, next) => {
+  if (req.params.id !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: 'You can only modify your own account.' })
+  }
+  next()
+}
+
+export default verifyOwnership
