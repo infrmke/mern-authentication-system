4285345e0e5b4fa2d8b3850744eca4a47fea683c
Inaros Lemuel <infromke@gmail.com>
2026-01-10 02:38:36 -0300

    isolate validation logic from validators as independent middleware

---
+++ backend/src/middlewares/handleValidation.js
@@ -0,0 +1,19 @@
+import { validationResult } from 'express-validator'
+import throwHttpError from '../utils/throwHttpError.js'
+
+/**
+ * Processa os resultados das validações do express-validator.
+ * Caso existam erros, interrompe a requisição e lança um erro formatado.
+ */
+const handleValidation = (req, res, next) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array()[0].msg // apenas a primeira mensagem de erro
+    throwHttpError(400, firstError, 'VALIDATION_ERROR')
+  }
+
+  next()
+}
+
+export default handleValidation
