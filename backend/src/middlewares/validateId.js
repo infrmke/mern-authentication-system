c3ef3525852ae852f7aa0c049fe087983cc5d06a
Inaros Lemuel <infromke@gmail.com>
2026-01-13 03:43:22 -0300

    update: middleware to only check if id follows ObjectId format

---
+++ backend/src/middlewares/validateId.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose'
+import throwHttpError from '../utils/throwHttpError.js'
+
+/**
+ * Verifica se o ID fornecido segue o padrão de ID auto-gerado pelo MongoDB.
+ */
+const validateId = async (req, res, next) => {
+  const { id } = req.params
+
+  const isIdValid = mongoose.Types.ObjectId.isValid(id)
+
+  if (!isIdValid) {
+    throwHttpError(
+      400,
+      'The provided ID format is invalid.',
+      'INVALID_ID_FORMAT'
+    )
+  }
+
+  next()
+}
+
+export default validateId
