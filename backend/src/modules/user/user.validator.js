a41bdd2da63fddd8eb9715d036f607c9d82b72c3
Inaros Lemuel <infromke@gmail.com>
2026-01-13 05:23:36 -0300

    remove unused "param" import

--- backend/src/modules/user/user.validator.js
+++ backend/src/modules/user/user.validator.js
@@ -1,4 +1,4 @@
-import { body, param } from 'express-validator'
+import { body } from 'express-validator'
 import handleValidation from '../../middlewares/handleValidation.js'
 
 const registerValidator = [
