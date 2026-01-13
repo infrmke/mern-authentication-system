a5fb61746a5aef62d0c88e6622615b2ef58b1533
Inaros Lemuel <infromke@gmail.com>
2026-01-13 04:03:00 -0300

    create validator for user updating

--- backend/src/modules/user/user.validator.js
+++ backend/src/modules/user/user.validator.js
@@ -36,4 +36,43 @@ const registerValidator = [
   handleValidation,
 ]
 
-export default registerValidator
+const updateValidator = [
+  body('name')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Name cannot be empty.')
+    .isLength({ min: 2, max: 56 })
+    .withMessage('Name must be between 2 and 56 characters.'),
+
+  body('email')
+    .optional()
+    .trim()
+    .normalizeEmail() // retira pontos extras e converte a string para minúscula
+    .notEmpty()
+    .withMessage('Email cannot be empty.')
+    .isEmail()
+    .withMessage('Provide a valid email address.'),
+
+  body('password')
+    .optional()
+    .notEmpty()
+    .withMessage('Password cannot be empty.')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters.'),
+
+  body('confirm_password')
+    .if(body('password').exists()) // apenas valida se "password" for enviado
+    .notEmpty()
+    .withMessage('Confirm your password.')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords do not match each other.')
+      }
+      return true
+    }),
+
+  handleValidation,
+]
+
+export { registerValidator, updateValidator }
