ce9714d7f78dc4d5efe907eeb5d930624684a0f9
Inaros Lemuel <infromke@gmail.com>
2026-01-13 21:56:25 -0300

    consolidate access-oriented middleware combos into a single file

---
+++ backend/src/middlewares/tollPlaza.js
@@ -0,0 +1,37 @@
+import validateId from './validateId.js'
+import verifyAccessToken from './verifyAccessToken.js'
+import isAccountVerified from './isAccountVerified.js'
+import verifyOwnership from './verifyOwnership.js'
+
+/**
+ * Verifica se o usuário está logado e se sua conta foi verificada.
+ */
+const verifiedOnly = [verifyAccessToken, isAccountVerified]
+
+/**
+ * Verifica se o usuário está logado e se é dono da conta que deseja alterar.
+ */
+const ownerOnly = [verifyAccessToken, verifyOwnership]
+
+/**
+ * Verifica se o usuário está logado e se sua conta foi verificada.
+ * Também verifica se o ID (`id`) passado é válido e se o usuário logado é dono da conta que deseja alterar.
+ */
+const fullLock = [
+  verifyAccessToken,
+  isAccountVerified,
+  validateId,
+  verifyOwnership,
+]
+
+/**
+ * Verifica se tipo (`type`) do otp enviado é `VERIFY` ou `RESET`.
+ * Se for do tipo VERIFY, o usuário passa pelo middleware verifyAccessToken.
+ * Se for do tipo RESET, o validator verifica e valida o `email` passado.
+ */
+const resendOtpFlow = (req, res, next) => {
+  if (req.body.type === 'VERIFY') return verifyAccessToken(req, res, next)
+  next()
+}
+
+export { verifiedOnly, ownerOnly, fullLock, resendOtpFlow }
