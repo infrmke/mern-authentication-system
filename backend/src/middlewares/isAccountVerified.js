e0e4a563dc21db86d3aff8a4b03a85ab56d2cd3b
Inaros Lemuel <infromke@gmail.com>
2026-01-14 17:14:31 -0300

    refactor: update middleware to fetch user by id and check account status

--- backend/src/middlewares/isAccountVerified.js
+++ backend/src/middlewares/isAccountVerified.js
@@ -1,17 +1,31 @@
+import UserService from '../modules/user/user.service.js'
 import throwHttpError from '../utils/throwHttpError.js'
 
 /**
  * Restringe o acesso apenas a usuários que realizaram a verificação de conta.
  */
-const isAccountVerified = (req, res, next) => {
-  if (!req.user.isAccountVerified) {
-    throwHttpError(
-      403,
-      'Account must be verified to perform this action.',
-      'USER_NOT_VERIFIED'
-    )
+const isAccountVerified = async (req, res, next) => {
+  const { id } = req.user
+
+  try {
+    const capsule = await UserService.findUserById(id)
+
+    if (!capsule) {
+      throwHttpError(404, 'User not found.', 'USER_NOT_FOUND')
+    }
+
+    if (!capsule.user.isAccountVerified) {
+      throwHttpError(
+        403,
+        'Account must be verified to perform this action.',
+        'USER_NOT_VERIFIED'
+      )
+    }
+
+    next()
+  } catch (error) {
+    next(error)
   }
-  next()
 }
 
 export default isAccountVerified
