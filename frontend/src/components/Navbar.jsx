30ec04cfb0a4a93c40065b40091a1715256da129
Inaros Lemuel <infromke@gmail.com>
2026-01-15 16:39:07 -0300

    feature: add minimal accessibility (ARIA)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,29 +18,44 @@ const Navbar = () => {
       setUserData(null)
       navigate('/')
     } catch (error) {
-      toast.error(
-        error?.response?.data['message'] || "Something didn't work. Try again."
-      )
+      toast.error(error?.response?.data['message'] || "Something didn't work. Try again.")
     }
   }
 
+  const firstName = userData?.name.split(' ')[0] || 'User'
+  const firstNameLetter = firstName[0].toUpperCase()
+
   return (
-    <nav className="nav">
-      <Link to="/home" className="nav__logo">
+    <nav className="nav" role="navigation" aria-label="Main Navigation">
+      <Link to="/home" className="nav__logo" aria-label="Go to Home Page">
         <img src={Logo} alt="Logo Ipsum Network" />
       </Link>
 
-      <ul className="menu">
-        <li className="menu__user">
-          <p className="menu__avatar">{userData?.name[0].toUpperCase()}</p>
-          <p>{userData?.name.split(' ')[0]}</p>
+      <ul className="menu" role="menubar">
+        <li
+          className="menu__user"
+          role="menuitem"
+          tabIndex="0"
+          aria-haspopup="true"
+          aria-label={`User profile: ${firstName}`}
+        >
+          <p className="menu__avatar" aria-hidden="true">
+            {firstNameLetter}
+          </p>
+          <p>{firstName}</p>
 
-          <span className="menu__icon">
+          <span className="menu__icon" aria-hidden="true">
             <ChevronDown color="hsl(220, 10%, 46%)" />
           </span>
         </li>
 
-        <li className="menu__item" onClick={handleClickLogOut}>
+        <li
+          className="menu__item"
+          role="menuitem"
+          tabIndex="0"
+          onClick={handleClickLogOut}
+          onKeyDown={(e) => e.key === 'Enter' && handleClickLogOut()}
+        >
           Log out
         </li>
       </ul>
