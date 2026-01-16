d02e197bde4f9068cf40501653ee23fee175f66c
Inaros Lemuel <infromke@gmail.com>
2026-01-15 21:27:49 -0300

    refactor: implement pagination in findAllUsers method

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -5,16 +5,25 @@ import generateToken from '../../utils/generateToken.js'
 import { sendEmail } from '../../config/nodemailer.js'
 import { getWelcomeMailOptions } from '../../utils/generateMail.js'
 
-const findAllUsers = async () => {
-  const users = await UserRepository.findAll()
+const findAllUsers = async (page = 1, limit = 10) => {
+  const offset = (page - 1) * limit
+
+  const { users, totalItems } = await UserRepository.findAll(limit, offset)
 
   if (!users) throwHttpError(500, 'Could not retrieve users.')
-
   if (users.length === 0) return null
 
   const formattedUsers = users.map((user) => formatUserObject(user))
 
-  return formattedUsers
+  // se existir 11 usuários e o limit for 10, haverá 2 páginas
+  const totalPages = Math.ceil(totalItems / limit)
+
+  return {
+    users: formattedUsers,
+    totalItems,
+    totalPages,
+    currentPage: page,
+  }
 }
 
 const findUser = async (filter, projection = undefined) => {
@@ -44,11 +53,7 @@ const createUser = async (data) => {
 
   const formattedUser = formatUserObject(user)
 
-  const accessToken = generateToken(
-    { id: user._id },
-    process.env.JWT_ACCESS_SECRET,
-    '1d'
-  )
+  const accessToken = generateToken({ id: user._id }, process.env.JWT_ACCESS_SECRET, '1d')
 
   const mail = getWelcomeMailOptions(data.name, data.email)
   await sendEmail(mail)
