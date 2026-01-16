53e602d15acc134df63268cfa6f4ddbba8c71d99
Inaros Lemuel <infromke@gmail.com>
2026-01-15 22:51:26 -0300

    refactor: rename otp repository methods

--- backend/src/modules/otp/otp.repository.js
+++ backend/src/modules/otp/otp.repository.js
@@ -1,10 +1,10 @@
 import Otp from './otp.model.js'
 
-const createOtp = async (options) => await Otp.create(options)
+const create = async (options) => await Otp.create(options)
 
-const findOtpById = async (id, type) =>
+const findById = async (id, type) =>
   await Otp.findOne({ user: id, type, expiresAt: { $gt: Date.now() } })
 
-const deleteOtp = async (id, type) => await Otp.deleteOne({ user: id, type })
+const remove = async (id, type) => await Otp.deleteOne({ user: id, type })
 
-export default { createOtp, findOtpById, deleteOtp }
+export default { create, findById, remove }
