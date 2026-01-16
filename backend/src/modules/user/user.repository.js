9d14fec5042760a51aaa6d7119e6bb8c4ca464cb
Inaros Lemuel <infromke@gmail.com>
2026-01-15 21:27:28 -0300

    refactor: implement pagination in findAll method

--- backend/src/modules/user/user.repository.js
+++ backend/src/modules/user/user.repository.js
@@ -1,16 +1,21 @@
 import User from '../user/user.model.js'
 
-const findAll = async () => await User.find()
+const findAll = async (limit, offset) => {
+  const [users, totalItems] = await Promise.all([
+    User.find().skip(offset).limit(limit),
+    User.countDocuments(),
+  ])
 
-const findOne = async (filter, projection = undefined) =>
-  await User.findOne(filter, projection)
+  return { users, totalItems }
+}
+
+const findOne = async (filter, projection = undefined) => await User.findOne(filter, projection)
 
 const findById = async (id) => await User.findById(id)
 
 const create = async (data) => await User.create(data)
 
-const updateById = async (id, data) =>
-  await User.findByIdAndUpdate(id, data, { new: true })
+const updateById = async (id, data) => await User.findByIdAndUpdate(id, data, { new: true })
 
 const deleteById = async (id) => await User.findByIdAndDelete(id)
 
