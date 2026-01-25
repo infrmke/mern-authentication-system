37dfa18242ed8b2f80a0087d41f6123f2c0409d8
Inaros Lemuel <infromke@gmail.com>
2026-01-25 05:27:33 -0300

    refactor: convert service functions to class methods in OtpService, SessionService, and UserService

--- backend/src/modules/user/user.service.js
+++ backend/src/modules/user/user.service.js
@@ -5,87 +5,88 @@ import generateToken from '../../utils/generateToken.js'
 import { sendEmail } from '../../config/nodemailer.js'
 import { getWelcomeMailOptions } from '../../utils/generateMail.js'
 
-const list = async (page = 1, limit = 10) => {
-  const offset = (page - 1) * limit
+class UserService {
+  #userRepository
 
-  const { users, totalItems } = await UserRepository.findAll(limit, offset)
+  constructor(userRepository) {
+    this.#userRepository = userRepository
+  }
 
-  if (!users) throwHttpError(500, 'Could not retrieve users.')
-  if (users.length === 0) return null
+  list = async (page = 1, limit = 10) => {
+    const offset = (page - 1) * limit
 
-  const formattedUsers = users.map((user) => formatUserObject(user))
+    const { users, totalItems } = await this.#userRepository.findAll(limit, offset)
 
-  // se existir 11 usuários e o limit for 10, haverá 2 páginas
-  const totalPages = Math.ceil(totalItems / limit)
+    if (!users) throwHttpError(500, 'Could not retrieve users.')
+    if (users.length === 0) return null
 
-  return {
-    users: formattedUsers,
-    totalItems,
-    totalPages,
-    currentPage: page,
+    const formattedUsers = users.map((user) => formatUserObject(user))
+
+    // se existir 11 usuários e o limit for 10, haverá 2 páginas
+    const totalPages = Math.ceil(totalItems / limit)
+
+    return {
+      users: formattedUsers,
+      totalItems,
+      totalPages,
+      currentPage: page,
+    }
+  }
+
+  find = async (filter, projection = {}) => {
+    const user = await this.#userRepository.findOne(filter, projection)
+
+    if (!user) return null
+
+    const formattedUser = formatUserObject(user)
+
+    return { user, formattedUser }
+  }
+
+  show = async (id) => {
+    const user = await this.#userRepository.findById(id)
+
+    if (!user) return null
+
+    const formattedUser = formatUserObject(user)
+
+    return { user, formattedUser }
+  }
+
+  store = async (data) => {
+    const user = await this.#userRepository.create(data)
+
+    if (!user) throwHttpError(500, 'Could not create user.')
+
+    const formattedUser = formatUserObject(user)
+
+    const accessToken = generateToken({ id: user._id }, process.env.JWT_ACCESS_SECRET, '1d')
+
+    const mail = getWelcomeMailOptions(data.name, data.email)
+    await sendEmail(mail)
+
+    return { formattedUser, accessToken }
+  }
+
+  update = async (id, data) => {
+    const user = await this.#userRepository.update(id, data)
+
+    if (!user) return null
+
+    const formattedUser = formatUserObject(user)
+
+    return formattedUser
+  }
+
+  destroy = async (id) => {
+    const user = await this.#userRepository.remove(id)
+
+    if (!user) return null
+
+    const formattedUser = formatUserObject(user)
+
+    return formattedUser
   }
 }
 
-const find = async (filter, projection = undefined) => {
-  const user = await UserRepository.findOne(filter, projection)
-
-  if (!user) return null
-
-  const formattedUser = formatUserObject(user)
-
-  return { user, formattedUser }
-}
-
-const show = async (id) => {
-  const user = await UserRepository.findById(id)
-
-  if (!user) return null
-
-  const formattedUser = formatUserObject(user)
-
-  return { user, formattedUser }
-}
-
-const store = async (data) => {
-  const user = await UserRepository.create(data)
-
-  if (!user) throwHttpError(500, 'Could not create user.')
-
-  const formattedUser = formatUserObject(user)
-
-  const accessToken = generateToken({ id: user._id }, process.env.JWT_ACCESS_SECRET, '1d')
-
-  const mail = getWelcomeMailOptions(data.name, data.email)
-  await sendEmail(mail)
-
-  return { formattedUser, accessToken }
-}
-
-const update = async (id, data) => {
-  const user = await UserRepository.update(id, data)
-
-  if (!user) return null
-
-  const formattedUser = formatUserObject(user)
-
-  return formattedUser
-}
-
-const destroy = async (id) => {
-  const user = await UserRepository.remove(id)
-
-  if (!user) return null
-
-  const formattedUser = formatUserObject(user)
-
-  return formattedUser
-}
-
-export default {
-  list,
-  find,
-  show,
-  store,
-  update,
-  destroy,
-}
+export default new UserService(UserRepository)
