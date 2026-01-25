981bb4987ceaa29ae19898eff85d5a763661c29a
Inaros Lemuel <infromke@gmail.com>
2026-01-25 05:27:33 -0300

    refactor: convert service functions to class methods in OtpService, SessionService, and UserService

--- backend/src/modules/otp/otp.service.js
+++ backend/src/modules/otp/otp.service.js
@@ -9,130 +9,133 @@ import { createOtpOptions } from '../../utils/generateOtp.js'
 import { getOtpMailOptions } from '../../utils/generateMail.js'
 import { sendEmail } from '../../config/nodemailer.js'
 
-const sendVerification = async (id) => {
-  const capsule = await UserService.show(id)
+class OtpService {
+  #otpRepository
+  #userService
 
-  if (!capsule) return null
+  constructor(otpRepository, userService) {
+    this.#otpRepository = otpRepository
+    this.#userService = userService
+  }
 
-  const { user } = capsule
+  sendVerification = async (id) => {
+    const capsule = await this.#userService.show(id)
 
-  if (user.isAccountVerified)
-    throwHttpError(400, 'Account has already been verified.', 'USER_ALREADY_VERIFIED')
+    if (!capsule) return null
 
-  const otpOptions = createOtpOptions(user._id, 'VERIFY')
-  const newOtp = await OtpRepository.create(otpOptions)
+    const { user } = capsule
 
-  const mail = getOtpMailOptions(user.email, newOtp.code, 'VERIFY')
-  await sendEmail(mail)
+    if (user.isAccountVerified)
+      throwHttpError(400, 'Account has already been verified.', 'USER_ALREADY_VERIFIED')
 
-  const formattedUser = formatUserObject(user)
+    const otpOptions = createOtpOptions(user._id, 'VERIFY')
+    const newOtp = await this.#otpRepository.create(otpOptions)
 
-  return formattedUser
-}
+    const mail = getOtpMailOptions(user.email, newOtp.code, 'VERIFY')
+    await sendEmail(mail)
 
-const sendReset = async (filter) => {
-  const capsule = await UserService.find(filter)
+    const formattedUser = formatUserObject(user)
 
-  if (!capsule) return null
+    return formattedUser
+  }
 
-  const { user } = capsule
+  sendReset = async (filter) => {
+    const capsule = await this.#userService.find(filter)
 
-  const otpOptions = createOtpOptions(user._id, 'RESET')
-  const newOtp = await OtpRepository.create(otpOptions)
+    if (!capsule) return null
 
-  const mail = getOtpMailOptions(user.email, newOtp.code, 'RESET')
-  await sendEmail(mail)
+    const { user } = capsule
 
-  const formattedUser = formatUserObject(user)
+    const otpOptions = createOtpOptions(user._id, 'RESET')
+    const newOtp = await this.#otpRepository.create(otpOptions)
 
-  return formattedUser
-}
+    const mail = getOtpMailOptions(user.email, newOtp.code, 'RESET')
+    await sendEmail(mail)
 
-const resendCode = async (type, filter) => {
-  const capsule = await UserService.find(filter)
+    const formattedUser = formatUserObject(user)
 
-  if (!capsule) return null
+    return formattedUser
+  }
 
-  const { user } = capsule
+  resend = async (type, filter) => {
+    const capsule = await this.#userService.find(filter)
 
-  // deleta o OTP previamente gerado
-  await OtpRepository.remove(user._id, type)
+    if (!capsule) return null
 
-  if (type === 'VERIFY') {
-    return await sendVerification(user._id)
-  } else if (type === 'RESET') {
-    return await sendReset({ email: user.email })
+    const { user } = capsule
+
+    // deleta o OTP previamente gerado
+    await this.#otpRepository.remove(user._id, type)
+
+    if (type === 'VERIFY') {
+      return await this.sendVerification(user._id)
+    } else if (type === 'RESET') {
+      return await this.sendReset({ email: user.email })
+    }
+  }
+
+  validateEmail = async (id, otp, otpType) => {
+    const capsule = await this.#userService.show(id)
+
+    if (!capsule) return null
+
+    const { user } = capsule
+
+    if (user.isAccountVerified)
+      throwHttpError(400, 'Account has already been verified.', 'USER_ALREADY_VERIFIED')
+
+    const otpDocument = await this.#otpRepository.findById(user._id, 'VERIFY')
+
+    if (!otpDocument) throwHttpError(500, 'Could not verify code. Try again.')
+
+    if (!otpDocument.code || otp !== otpDocument.code || otpType !== otpDocument.type)
+      throwHttpError(403, 'Invalid code.', 'OTP_NOT_FOUND')
+
+    const updatedUser = await this.#userService.update(user._id, {
+      isAccountVerified: true,
+    })
+
+    if (!updatedUser) throwHttpError(500, 'Could not update user. Try again.')
+
+    await this.#otpRepository.remove(id, 'VERIFY')
+
+    return updatedUser
+  }
+
+  validateReset = async (otp, otpType, filter) => {
+    const capsule = await this.#userService.find(filter)
+
+    if (!capsule) throwHttpError(400, 'Verification failed. User not found.', 'USER_NOT_FOUND')
+
+    const { user } = capsule
+
+    const otpDocument = await this.#otpRepository.findById(user._id, 'RESET')
+
+    if (!otpDocument) throwHttpError(500, 'Could not verify code. Try again.')
+
+    if (!otpDocument.code || otp !== otpDocument.code || otpType !== otpDocument.type)
+      throwHttpError(403, 'Invalid code.', 'OTP_NOT_FOUND')
+
+    await this.#otpRepository.remove(user._id, 'RESET')
+
+    const passwordToken = generateToken({ id: user._id }, process.env.JWT_RESET_SECRET, '15m')
+
+    return passwordToken
+  }
+
+  resetPassword = async (filter, password) => {
+    const capsule = await this.#userService.find(filter, '+password')
+
+    if (!capsule) return null
+
+    const { user } = capsule
+
+    const updatedUser = await this.#userService.update(user._id, { password })
+
+    if (!updatedUser) throwHttpError(500, 'Could not update user. Try again.')
+
+    return updatedUser
   }
 }
 
-const validateEmail = async (id, otp, otpType) => {
-  const capsule = await UserService.show(id)
-
-  if (!capsule) return null
-
-  const { user } = capsule
-
-  if (user.isAccountVerified)
-    throwHttpError(400, 'Account has already been verified.', 'USER_ALREADY_VERIFIED')
-
-  const otpDocument = await OtpRepository.findById(user._id, 'VERIFY')
-
-  if (!otpDocument) throwHttpError(500, 'Could not verify code. Try again.')
-
-  if (!otpDocument.code || otp !== otpDocument.code || otpType !== otpDocument.type)
-    throwHttpError(403, 'Invalid code.', 'OTP_NOT_FOUND')
-
-  const updatedUser = await UserService.update(user._id, {
-    isAccountVerified: true,
-  })
-
-  if (!updatedUser) throwHttpError(500, 'Could not update user. Try again.')
-
-  await OtpRepository.remove(id, 'VERIFY')
-
-  return updatedUser
-}
-
-const validateReset = async (otp, otpType, filter) => {
-  const capsule = await UserService.find(filter)
-
-  if (!capsule) throwHttpError(400, 'Verification failed. User not found.', 'USER_NOT_FOUND')
-
-  const { user } = capsule
-
-  const otpDocument = await OtpRepository.findById(user._id, 'RESET')
-
-  if (!otpDocument) throwHttpError(500, 'Could not verify code. Try again.')
-
-  if (!otpDocument.code || otp !== otpDocument.code || otpType !== otpDocument.type)
-    throwHttpError(403, 'Invalid code.', 'OTP_NOT_FOUND')
-
-  await OtpRepository.remove(user._id, 'RESET')
-
-  const passwordToken = generateToken({ id: user._id }, process.env.JWT_RESET_SECRET, '15m')
-
-  return passwordToken
-}
-
-const resetPassword = async (filter, password) => {
-  const capsule = await UserService.find(filter, '+password')
-
-  if (!capsule) return null
-
-  const { user } = capsule
-
-  const updatedUser = await UserService.update(user._id, { password })
-
-  if (!updatedUser) throwHttpError(500, 'Could not update user. Try again.')
-
-  return updatedUser
-}
-
-export default {
-  sendVerification,
-  sendReset,
-  resendCode,
-  validateEmail,
-  validateReset,
-  resetPassword,
-}
+export default new OtpService(OtpRepository, UserService)
--- backend/src/modules/session/session.service.js
+++ backend/src/modules/session/session.service.js
@@ -5,34 +5,42 @@ import generateToken from '../../utils/generateToken.js'
 import { validatePassword } from '../../utils/password.js'
 import formatUserObject from '../../utils/formatUserObject.js'
 
-const verify = async (id) => {
-  const capsule = await UserService.show(id)
+class SessionService {
+  #userService
 
-  if (!capsule) return null
+  constructor(userService) {
+    this.#userService = userService
+  }
 
-  const { user } = capsule
+  verify = async (id) => {
+    const capsule = await this.#userService.show(id)
 
-  const formattedUser = formatUserObject(user)
+    if (!capsule) return null
 
-  return formattedUser
+    const { user } = capsule
+
+    const formattedUser = formatUserObject(user)
+
+    return formattedUser
+  }
+
+  authenticate = async (password, filter) => {
+    const capsule = await this.#userService.find(filter, '+password')
+
+    if (!capsule) return null
+
+    const { user } = capsule
+
+    const isPwdValid = await validatePassword(password, user.password)
+
+    if (!isPwdValid) throwHttpError(400, 'Invalid credentials.', 'USER_INVALID_CREDENTIALS')
+
+    const formattedUser = formatUserObject(user)
+
+    const accessToken = generateToken({ id: user._id }, process.env.JWT_ACCESS_SECRET, '1d')
+
+    return { formattedUser, accessToken }
+  }
 }
 
-const authenticate = async (password, filter) => {
-  const capsule = await UserService.find(filter, '+password')
-
-  if (!capsule) return null
-
-  const { user } = capsule
-
-  const isPwdValid = await validatePassword(password, user.password)
-
-  if (!isPwdValid) throwHttpError(400, 'Invalid credentials.', 'USER_INVALID_CREDENTIALS')
-
-  const formattedUser = formatUserObject(user)
-
-  const accessToken = generateToken({ id: user._id }, process.env.JWT_ACCESS_SECRET, '1d')
-
-  return { formattedUser, accessToken }
-}
-
-export default { verify, authenticate }
+export default new SessionService(UserService)
