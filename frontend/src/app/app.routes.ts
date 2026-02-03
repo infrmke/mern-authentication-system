5bd08f832ea7d8f6b24b7c702ab17d33e54255ce
Inaros Lemuel <infromke@gmail.com>
2026-02-03 19:12:27 -0300

    feat: define root path "/" as the app's login page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // raiz "/"
+    loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+    title: 'Log in | Authentication System',
+  },
+];
