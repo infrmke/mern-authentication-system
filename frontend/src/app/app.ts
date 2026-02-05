eb5f82bb9f08f12eb804fbc740ab20b223711dda
Inaros Lemuel <infromke@gmail.com>
2026-02-05 18:32:21 -0300

    feat: implement OnInit to check for existing user session before rendering app

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { AuthService } from './core/services/auth-service';
+import { UserService } from './core/services/user-service';
+import { PulseLoader } from './shared/components/loaders/pulse-loader/pulse-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet, PulseLoader],
   styleUrl: './app.scss',
+  template: `
+    @if (isReady()) {
+      <router-outlet></router-outlet>
+    } @else {
+      <app-pulse-loader></app-pulse-loader>
+    }
+  `,
 })
-export class App {}
+export class App implements OnInit {
+  private authService = inject(AuthService);
+  private userService = inject(UserService);
+
+  isReady = signal(false);
+
+  // verifica se o usuário já está logado na inicialização do app
+  ngOnInit() {
+    this.authService.verifySession().subscribe({
+      next: (user) => {
+        this.userService.setUserData(user);
+        this.isReady.set(true);
+      },
+      error: () => {
+        this.userService.clearUser();
+        this.isReady.set(true);
+      },
+    });
+  }
+}
