10030ddf0bcf49e580652a4259e879d77f848c6d
Inaros Lemuel <infromke@gmail.com>
2026-02-05 06:26:14 -0300

    feat: implement reactive form and auth service for login page

--- frontend/src/app/features/auth/login/login.ts
+++ frontend/src/app/features/auth/login/login.ts
@@ -1,17 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { CardBody } from '../../../shared/components/card-body/card-body';
 import { InputGroup } from '../../../shared/components/input-group/input-group';
 import { RedirectAction } from '../../../shared/components/redirect-action/redirect-action';
+import { AuthService } from '../../../core/services/auth-service';
+import { UserService } from '../../../core/services/user-service';
 
 @Component({
   selector: 'app-login',
-  imports: [CardBody, InputGroup, RedirectAction],
+  imports: [CardBody, InputGroup, RedirectAction, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  private formBuilder = inject(NonNullableFormBuilder);
+  private authService = inject(AuthService);
+  private userService = inject(UserService);
+  private router = inject(Router);
+
+  isLoading = signal(false);
+
+  form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
   onLoginSubmit(): void {
-    console.log('Enviado.');
+    if (this.form.invalid) {
+      this.form.markAllAsTouched(); // faz aparecer as mensagens de erro nos inputs
+      return;
+    }
+
+    const formData = this.form.getRawValue();
+
+    this.isLoading.set(true);
+
+    this.authService.login(formData).subscribe({
+      next: (user) => {
+        this.userService.setUserData(user);
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        this.isLoading.set(false);
+        alert(err.error?.message || "Something didn't work! Try again.");
+      },
+      complete: () => this.isLoading.set(false),
+    });
   }
 }
