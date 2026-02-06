37e846a4feb4d5dc02ac411bcfcde10ae7559962
Inaros Lemuel <infromke@gmail.com>
2026-02-06 01:05:36 -0300

    feat: add logic for loader selection based on the current route

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,19 +1,24 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, NavigationStart, Router, RouterOutlet } from '@angular/router';
 
 import { AuthService } from './core/services/auth-service';
 import { UserService } from './core/services/user-service';
 import { PulseLoader } from './shared/components/loaders/pulse-loader/pulse-loader';
+import { SpinnerLoader } from './shared/components/loaders/spinner-loader/spinner-loader';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PulseLoader],
+  imports: [RouterOutlet, SpinnerLoader, PulseLoader],
   styleUrl: './app.scss',
   template: `
     @if (isReady()) {
       <router-outlet></router-outlet>
     } @else {
-      <app-pulse-loader></app-pulse-loader>
+      @if (loaderType === 'pulse') {
+        <app-pulse-loader></app-pulse-loader>
+      } @else {
+        <app-spinner-loader></app-spinner-loader>
+      }
     }
   `,
 })
@@ -22,6 +27,13 @@ export class App implements OnInit {
   private userService = inject(UserService);
 
   isReady = signal(false);
+
+  // verifica a rota atual e distingue entre qual loader utilizar
+  get loaderType(): 'pulse' | 'spinner' {
+    const path = window.location.pathname;
+    const publicPaths = ['/', '/register', '/forgot-password']; // rotas cujo loader deve ser o pulse
+    return publicPaths.includes(path) ? 'pulse' : 'spinner';
+  }
 
   // verifica se o usuário já está logado na inicialização do app
   ngOnInit() {
