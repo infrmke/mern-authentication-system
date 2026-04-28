49c4b35135d57e5db784236675bd26cb1ce7b885
Inaros Lemuel <infromke@gmail.com>
2026-04-27 23:54:35 -0300

    feat: create isAuthenticated and isGuest middlewares for access control

---
+++ backend/src/middlewares/isLoggedIn.js
@@ -0,0 +1,23 @@
+import throwHttpError from '../utils/throwHttpError.js'
+
+/**
+ * Impede que usuários já autenticados acessem a rota POST `/sessions/login`.
+ */
+const isAuthenticated = (req, res, next) => {
+  if (req.cookies.accessToken) {
+    return next(throwHttpError(400, 'You are already logged in.', 'USER_ALREADY_AUTHENTICATED'))
+  }
+  next()
+}
+
+/**
+ * Impede que usuários já autenticados acessem as rotas de POST `/users` e `/otps/password-reset/request`.
+ */
+const isGuest = (req, res, next) => {
+  if (req.cookies.accessToken) {
+    return next(throwHttpError(403, 'Cannot proceed while logged in.', 'FORBIDDEN_ACCESS'))
+  }
+  next()
+}
+
+export { isAuthenticated, isGuest }
