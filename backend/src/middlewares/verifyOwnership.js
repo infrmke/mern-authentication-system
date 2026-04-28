66d38a41bf7182cffdcaa347e508c4a821b1ad19
Inaros Lemuel <infromke@gmail.com>
2026-04-28 15:39:24 -0300

    refactor: simplify error handling with throwHttpError utility

--- backend/src/middlewares/verifyOwnership.js
+++ backend/src/middlewares/verifyOwnership.js
@@ -1,12 +1,11 @@
+import throwHttpError from '../utils/throwHttpError'
+
 /**
  * Verifica se o usuário autenticado é o proprietário da conta passada pelo ID na URL.
  */
 const verifyOwnership = (req, res, next) => {
-  if (req.params.id !== req.user.id.toString()) {
-    return res
-      .status(403)
-      .json({ message: 'You can only modify your own account.' })
-  }
+  if (req.params.id !== req.user.id.toString())
+    throwHttpError(403, 'You can only modify your own account.', 'FORBIDDEN_ACTION')
   next()
 }
 
